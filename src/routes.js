54826da9fb8a0adaecc697bf41c5662dee013d50
junior de oliveira belem <junioroliveira.belem@gmail.com>
2022-11-03 22:03:25 -0300

    F

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import { Navigate, useRoutes } from 'react-router-dom';
 // layouts
 import DashboardLayout from './layouts/dashboard';
-import LogoOnlyLayout from './layouts/LogoOnlyLayout';
+
 //
 import Blog from './pages/Blog';
 import User from './pages/GridUser/User';
