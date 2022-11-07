ebecd02ee70459f6937815f5b7d7cee95ad10a09
junior de oliveira belem <junioroliveira.belem@gmail.com>
2022-11-07 11:04:37 -0300

    update perfil

--- src/_mock/account.js
+++ src/_mock/account.js
@@ -1,9 +1,9 @@
 // ----------------------------------------------------------------------
 
 const account = {
-  displayName: 'Jaydon Frankie',
-  email: 'demo@minimals.cc',
-  photoURL: '/static/mock-images/avatars/avatar_default.jpg',
+  displayName: 'Junior Oliveira',
+  email: 'demo@junior.cc',
+  photoURL: 'https://media-exp1.licdn.com/dms/image/C4D03AQHcbFe9-Phe1Q/profile-displayphoto-shrink_800_800/0/1656433703054?e=1673481600&v=beta&t=0BQ4CcNHqAfALp3abBGJSnPZLFjZ6vq2jAMfvrvlt8Y',
 };
 
 export default account;
