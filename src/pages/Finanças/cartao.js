10d8ef15bf2950f5d5d6b40b52ae39e56f77f02d
junior de oliveira belem <junioroliveira.belem@gmail.com>
2022-11-19 18:15:20 -0300

    Correção build Netlify

--- src/pages/Finanças/cartao.js
+++ src/pages/Finanças/cartao.js
@@ -9,16 +9,16 @@ import Iconify from '../../components/Iconify';
 
 
 
-const IconWrapperStyle = styled('div')(({ theme }) => ({
-    margin: 'auto',
-    display: 'flex',
-    borderRadius: '50%',
-    alignItems: 'center',
-    width: theme.spacing(8),
-    height: theme.spacing(8),
-    justifyContent: 'center',
-    marginBottom: theme.spacing(3),
-}));
+// const IconWrapperStyle = styled('div')(({ theme }) => ({
+//     margin: 'auto',
+//     display: 'flex',
+//     borderRadius: '50%',
+//     alignItems: 'center',
+//     width: theme.spacing(8),
+//     height: theme.spacing(8),
+//     justifyContent: 'center',
+//     marginBottom: theme.spacing(3),
+// }));
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: 'transparent',
     ...theme.typography.body,
