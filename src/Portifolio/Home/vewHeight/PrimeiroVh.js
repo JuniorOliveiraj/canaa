794ce1e24f50d123d7222f2b53c4345e1e072f1d
junior de oliveira belem <junioroliveira.belem@gmail.com>
2022-12-05 13:01:42 -0300

    Teste de Designer

--- src/Portifolio/Home/vewHeight/PrimeiroVh.js
+++ src/Portifolio/Home/vewHeight/PrimeiroVh.js
@@ -1,5 +1,5 @@
 
-import { Box, Paper,Grid } from "@mui/material";
+import { Box, Paper, Grid } from "@mui/material";
 import { styled } from '@mui/material/styles';
 import React from "react";
 import { Carrousel, Slides, Slide, SlideNav } from "./Carrousel";
@@ -10,64 +10,73 @@ const Item = styled(Paper)(({ theme }) => ({
     ...theme.typography.body2,
     padding: theme.spacing(1),
     textAlign: 'center',
-    color:'#ffffff',
-
+    color: '#ffffff',
     fontFamily: 'Work Sans',
-
-
-
+}));
+const ImagensStyle = styled('div')(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    height:'80vh'
 
 }));
-
 export default function PrimeiroVH() {
     console.log(imgs)
     return (
         <Box sx={{ width: '100%', margin: 0, maxHeight: '90vh' }}>
+
+
+
+
+
             <Carrousel bsv>
                 <Slides>
                     {imgs.map((index) => (
-                        <Slide key={index.text}>
-                            <Item sx={{
-                                backgroundImage: ` url(${index.img})`,
-
-                                /* Full height */
-                                height: "100%",
-
-                                /* Center and scale the image nicely */
-                                backgroundPosition: 'center',
-                                backgroundRepeat: 'no-repeat',
-                                backgroundSize: 'cover'
-                            }}>
-                                
-                                <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} sx={{ width: '100%', margin: 0 }}>
-                                    <Grid  container xs={6} sx={{margin:5 , marginLeft:10}}>
+                        <Slide key={index.text}><Item sx={{
+                            backgroundImage: ` url(${index.img})`,
+                            height: "100%",
+                            backgroundPosition: 'center',
+                            backgroundRepeat: 'no-repeat',
+                            backgroundSize: 'cover'
+                        }}>
+                            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} sx={{ width: '100%', margin: 0 }}>
+                                <Grid container xs={6} sx={{ margin: 5, marginLeft: 10 }}>
                                     <div className="col-md-6">
-                                    <div className="slider_item-detail">
-                                        <div>
-                                            <h1>
-                                                {index.title}
-                                            </h1>
-                                            <p>
-                                                {index.Text}
-                                            </p>
-  
+                                        <div className="slider_item-detail">
+                                            <div>
+                                                <h1>
+                                                    {index.title}
+                                                </h1>
+                                                <p>
+                                                    {index.Text}
+                                                </p>
+
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                    </Grid>
-                                    <Grid xs={5} sx={{opacity:0}}>
-                                        <Item><img  src="https://media-exp1.licdn.com/dms/image/C4D03AQHcbFe9-Phe1Q/profile-displayphoto-shrink_800_800/0/1656433703054?e=1675900800&v=beta&t=gkHNijmPykUUp3N2Uml0QesKL_nq3jf9KcRz_szznoo"/></Item>
-                                    </Grid>
-
                                 </Grid>
-                            </Item>
+                                <Grid xs={5} >
+                                    <Item><ImagensStyle
+                                     sx={{
+                                        backgroundImage: ` url(${index.perfil})`,
+                                        
+                                        backgroundPosition: 'center',
+                                        backgroundRepeat: 'no-repeat',
+                                        backgroundSize: 'cover'
+                                    }}
+                                     
+                                     /></Item>
+                                </Grid>
+                            </Grid>
+                        </Item>
                         </Slide>
                     ))
                     }
                 </Slides>
-                <SlideNav classNameName="SlideNav" navType="bullet" />
+                <SlideNav className="SlideNav" navType="bullet" />
             </Carrousel>
+
         </Box >
 
     )
 }
+
--- src/Portifolio/Home/vewHeight/imagens.js
+++ src/Portifolio/Home/vewHeight/imagens.js
@@ -1,22 +1,27 @@
 const imgs = [{
-    img:'https://media-exp1.licdn.com/dms/image/C4E16AQF7u2JBw5H92w/profile-displaybackgroundimage-shrink_350_1400/0/1656415668138?e=1675900800&v=beta&t=JCq730gqMhvGBJNEPxnRPaaGZFlNgP4jE6VV6lML0Yk',
+    img:`../static/mock-images/imageHome/img_${4}.jpg`,
     Text:'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable.',
-    title:'junior Oliveira 1'
+    title:'junior Oliveira 1',
+    perfil:'https://media-exp1.licdn.com/dms/image/C4D03AQHcbFe9-Phe1Q/profile-displayphoto-shrink_800_800/0/1656433703054?e=1675900800&v=beta&t=gkHNijmPykUUp3N2Uml0QesKL_nq3jf9KcRz_szznoo'
 },{
-    img:'https://media-exp1.licdn.com/dms/image/C4E16AQF7u2JBw5H92w/profile-displaybackgroundimage-shrink_350_1400/0/1656415668138?e=1675900800&v=beta&t=JCq730gqMhvGBJNEPxnRPaaGZFlNgP4jE6VV6lML0Yk',
+    img:`../static/mock-images/imageHome/img_${2}.jpg`,
     Text:'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable.',
-    title:'Junior Oliveira 2'
+    title:'Junior Oliveira 2',
+    perfil:'https://avatars.githubusercontent.com/u/85002295?v=4'
 },{
     img:'https://media.giphy.com/media/4rfTQbT3PKCw8/giphy.gif',
     Text:'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable.',
-    title:'Junior Oliveira 3'
+    title:'Junior Oliveira 3',
+    perfil:'https://www.infomoney.com.br/wp-content/uploads/2022/01/FJkLjuCXMAYC1MO.jpg?fit=1280%2C720&quality=50&strip=all'
 },{
     img:'https://media.giphy.com/media/siREjlLKjdSZq/giphy.gif',
     Text:'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable.',
-    title:'Junior Oliveira 4'
+    title:'Junior Oliveira 4',
+    perfil:'https://comoinvestir.thecap.com.br/medias/2022/01/vendas-de-nfts-do-bored-ape-yacht-club-ultrapassam-us-1-bilhao.webp'
 },{
     img:'https://media.giphy.com/media/A8NNZlVuA1LoY/giphy.gif',
     Text:'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable.',
-    title:'Junior Oliveira 5'
+    title:'Junior Oliveira 5',
+    perfil:'https://comoinvestir.thecap.com.br/medias/2022/01/vendas-de-nfts-do-bored-ape-yacht-club-ultrapassam-us-1-bilhao.webp'
 }]
 export default imgs
