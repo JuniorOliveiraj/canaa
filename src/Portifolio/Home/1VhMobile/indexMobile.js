451556e84be74a52bb5e54873698690890d8d253
junior de oliveira belem <junioroliveira.belem@gmail.com>
2022-12-13 20:09:43 -0300

    Teste de Build

--- src/Portifolio/Home/1VhMobile/indexMobile.js
+++ src/Portifolio/Home/1VhMobile/indexMobile.js
@@ -1,48 +1,112 @@
-import { motion } from "framer-motion"
 
-import { styled } from "@mui/material";
-import imgs from "../vewHeight/imagens"
-import { useState, useEffect, useRef } from "react";
-import './stylle.css'
 
-const Boxcarrocel = styled('div')(({ theme }) => ({
-    maxHeight: '900px',
-    width: '100%',
-    margin: 0,
-    minHeight: '100vh',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'red'
 
-}))
-export default function PrimeiroMobile() {
-    const carrossel = useRef();
-    const [width, setWidth] = useState(null)
-    useEffect(() => {
-       console.log(carrossel.current?.scrollWidth, carrossel.current?.offsetWidth)
-   
-        setWidth(carrossel.current?.scrollWidth - carrossel.current?.offsetWidth)
-    }, []);
-    return (
-            <Boxcarrocel  >
-                <motion.div className="CarroselMobile" ref={carrossel} whileTap={{cursor:'grabbing'}}>
-                    <motion.div className="innerCarrosel"
-                    drag='x'
-                    dragConstraints={{right:0, left: -width}}
-                    >
-                        {
-                            imgs.map(index =>(
-                              
-                                <motion.div className="itemImg">
-                                    {/* <img src={index.perfil} alt={index.title} key={index.id}/> */}
-                                    {index.Text}
-                                </motion.div>
-                             
-                            ))
-                        }
-                    </motion.div>
-                </motion.div>
-            </Boxcarrocel>
-    )
-}
+
+import'./stylle.css';
+import imgs from "../vewHeight/imagens";
+import * as React from "react";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { wrap } from "popmotion";
+import { styled } from '@mui/material/styles';
+
+
+const Container = styled('div')(({ theme }) => ({
+  ...theme.typography.body2,
+  overflowY:'hidden'
+
+
+}));
+
+const variants = {
+  enter: (direction: number) => {
+    return {
+      x: direction > 0 ? 1000 : -1000,
+      opacity: 0
+    };
+    
+  },
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1
+  },
+  exit: (direction: number) => {
+    return {
+      zIndex: 0,
+      x: direction < 0 ? 1000 : -1000,
+      opacity: 0
+    };
+  }
+};
+
+/**
+ * Experimenting with distilling swipe offset and velocity into a single variable, so the
+ * less distance a user has swiped, the more velocity they need to register as a swipe.
+ * Should accomodate longer swipes and short flicks without having binary checks on
+ * just distance thresholds and velocity > 0.
+ */
+const swipeConfidenceThreshold = 10000;
+const swipePower = (offset: number, velocity: number) => {
+  return Math.abs(offset) * velocity;
+};
+  const images = []
+  imgs.forEach(function(e){
+    images.push(e.img)
+  })
+const PrimeiroMobile = () => {
+
+
+
+
+  const [[page, direction], setPage] = useState([0, 0]);
+
+  // We only have 3 images, but we paginate them absolutely (ie 1, 2, 3, 4, 5...) and
+  // then wrap that within 0-2 to find our image ID in the array below. By passing an
+  // absolute page index as the `motion` component's `key` prop, `AnimatePresence` will
+  // detect it as an entirely new image. So you can infinitely paginate as few as 1 images.
+  const imageIndex = wrap(0, images.length, page);
+  const paginate = (newDirection) => {
+    setPage([page + newDirection, newDirection]);
+  };
+
+  return (
+    <Container>
+      <AnimatePresence initial={false} custom={direction} >
+        <motion.img
+          key={page}
+          src={images[imageIndex]}
+          custom={direction}
+          variants={variants}
+          initial="enter"
+          animate="center"
+          exit="exit"
+          transition={{
+            x: { type: "spring", stiffness: 300, damping: 30 },
+            opacity: { duration: 0.2 }
+          }}
+          drag="x"
+          dragConstraints={{ left: 0, right: 0 }}
+          dragElastic={1}
+          onDragEnd={(e, { offset, velocity }) => {
+            const swipe = swipePower(offset.x, velocity.x);
+
+            if (swipe < -swipeConfidenceThreshold) {
+              paginate(1);
+            } else if (swipe > swipeConfidenceThreshold) {
+              paginate(-1);
+            }
+          }}
+        />
+        
+      </AnimatePresence>
+      <div className="next" onClick={() => paginate(1)}>
+        {"‣"}
+      </div>
+      <div className="prev" onClick={() => paginate(-1)}>
+        {"‣"}
+      </div>
+    </Container>
+  );
+};
+export default PrimeiroMobile;
