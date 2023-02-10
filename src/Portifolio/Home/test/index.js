9808efe8a85c475c84273ed31ba70fa7ef4c9898
junior de oliveira belem <junioroliveira.belem@gmail.com>
2023-02-09 21:35:51 -0300

    Cerreção bug de ativos imagens

--- src/Portifolio/Home/test/index.js
+++ src/Portifolio/Home/test/index.js
@@ -9,7 +9,7 @@ export default function TEste() {
     return (
         <>
             <div className="options">
-                <div className={ !curse01 ?  "option active": "option"}  onClick={()=>{curse01 ?  setCurse01(false) : setCurse01(true); setCurse02(false); setCurse03(false)}} style={{backgroundImage:"url(https://66.media.tumblr.com/6fb397d822f4f9f4596dff2085b18f2e/tumblr_nzsvb4p6xS1qho82wo1_1280.jpg)"}}>
+                <div className={ !curse01 ?  "option active": "option"}  onClick={()=>{!curse01 && curse01 ?  setCurse01(false) : setCurse01(true); setCurse02(false); setCurse03(false)}} style={{backgroundImage:"url(https://66.media.tumblr.com/6fb397d822f4f9f4596dff2085b18f2e/tumblr_nzsvb4p6xS1qho82wo1_1280.jpg)"}}>
                     <div className="shadow"></div>
                     <div className="label">
                         <div className="icon">
@@ -21,7 +21,7 @@ export default function TEste() {
                         </div>
                     </div>
                 </div>
-                <div className={ !curse02 ?  "option active": "option"}  onClick={()=>{curse02 ?  setCurse02(false) : setCurse02(true) ; setCurse01(false); setCurse03(false) }}  style={{backgroundImage:"url(https://66.media.tumblr.com/8b69cdde47aa952e4176b4200052abf4/tumblr_o51p7mFFF21qho82wo1_1280.jpg)"}}>
+                <div className={ !curse02 ?  "option active": "option"}  onClick={()=>{!curse02 && curse02 ?  setCurse02(false) : setCurse02(true) ; setCurse01(false); setCurse03(false) }}  style={{backgroundImage:"url(https://66.media.tumblr.com/8b69cdde47aa952e4176b4200052abf4/tumblr_o51p7mFFF21qho82wo1_1280.jpg)"}}>
                     <div className="shadow"></div>
                     <div className="label">
                         <div className="icon">
@@ -33,7 +33,7 @@ export default function TEste() {
                         </div>
                     </div>
                 </div>
-                <div className={ !curse03 ?  "option active": "option"}  onClick={()=>{curse03 ?  setCurse03(false) : setCurse03(true);  setCurse02(false); setCurse01(false) }}  style={{backgroundImage:"url(https://66.media.tumblr.com/5af3f8303456e376ceda1517553ba786/tumblr_o4986gakjh1qho82wo1_1280.jpg)"}}>
+                <div className={ !curse03 ?  "option active": "option"}  onClick={()=>{!curse03 && curse03 ?  setCurse03(false) : setCurse03(true);  setCurse02(false); setCurse01(false) }}  style={{backgroundImage:"url(https://66.media.tumblr.com/5af3f8303456e376ceda1517553ba786/tumblr_o4986gakjh1qho82wo1_1280.jpg)"}}>
                     <div className="shadow"></div>
                     <div className="label">
                         <div className="icon">
