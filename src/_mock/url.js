e848b6a1737040dd917fe1d4a07f5c9142762096
junior de oliveira belem <85002295+JuniorOliveiraj@users.noreply.github.com>
2023-04-03 16:58:23 -0300

    Update url.js

--- src/_mock/url.js
+++ src/_mock/url.js
@@ -1,5 +1,6 @@
 // const urlApi = 'http://junior-oliveira.epizy.com/public'; 
-export const urlApi = 'http://junior-oliveira.epizy.com/public'; 
+var protocol = window.location.protocol;
+export const urlApi =  protocol + "//junior-oliveira.epizy.com/public/register";
 // export const urlApi = 'https://junioroliveiraj.000webhostapp.com'; 
 
 export default urlApi;
