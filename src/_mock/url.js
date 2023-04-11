33a9a72d49948f86765d9f4a7a13c85df4678598
junior de oliveira belem <junioroliveira.belem@gmail.com>
2023-04-10 22:09:26 -0300

    correção

--- src/_mock/url.js
+++ src/_mock/url.js
@@ -1,6 +1,6 @@
 // const urlApi = 'http://junior-oliveira.epizy.com/public'; 
 var protocol = window.location.protocol;
-export const urlApi ='http://localhost:8080' // protocol + "//junior-oliveira.epizy.com/public/register";
+export const urlApi = protocol + "//junior-oliveira.epizy.com/public/register"; //'http://localhost:8080' //
 // export const urlApi = 'https://junioroliveiraj.000webhostapp.com'; 
 
 export default urlApi;
