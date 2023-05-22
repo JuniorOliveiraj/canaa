49d70d6725bbcfbc4454bf33783aeeb24ac762e5
junior de oliveira belem <85002295+JuniorOliveiraj@users.noreply.github.com>
2023-05-22 13:03:28 -0300

    Update cardProduto.js

--- src/pages/noticiasAll/produtos/cardProduto.js
+++ src/pages/noticiasAll/produtos/cardProduto.js
@@ -27,6 +27,10 @@ const ProdutoList = ({ index, produtos, openTrue , produtosReload, setProdutosRe
                         <Typography component="div" variant="h5">
                             {produtos.name_produto}
                         </Typography>
+
+<Typography component="div" variant="h5">
+                            {produtos.id}
+                        </Typography>
                         <Typography
                             variant="subtitle1"
                             color="text.secondary"
