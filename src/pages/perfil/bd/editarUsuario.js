c7c3d270799838bc75e006942051193beea5cc5b
junior de oliveira belem <85002295+JuniorOliveiraj@users.noreply.github.com>
2023-06-07 12:57:16 -0300

    Add files via upload

---
+++ src/pages/perfil/bd/editarUsuario.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import urlApi from '../../../_mock/url';
+export default function editarUsusario(usuario, data,urlImg) {
+    return new Promise((resolve, reject) => {
+        console.log(usuario)
+        if (usuario) {
+            async function add() {
+                try {
+                    try {
+                        const caminho = '/users/update';
+                        const response = await axios.get(`${urlApi}${caminho}`, {
+                            params: {
+                                userID: usuario.uid,
+                                form: data,
+                                urlImg: urlImg,
+                            },
+                            headers: {
+                                authorization: usuario.accessToken,
+                            },
+                        });
+
+                        console.log(response);
+                        console.log(data.mensagen);
+                        resolve(response)
+
+                    } catch (error) {
+                        reject(error)
+                    }
+
+
+                } catch (error) {
+                    console.log("Erro ao fazer upload dos dados:", error);
+                }
+            }
+            add()
+        } else {
+            console.log('faça login');
+            reject(false)
+
+        }
+
+    })
+
+}
