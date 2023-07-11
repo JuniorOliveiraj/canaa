ac7286d117e54fdfee99bb100716e6135117b665
junior de oliveira belem <85002295+JuniorOliveiraj@users.noreply.github.com>
2023-07-11 09:00:45 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // routes
 import Router from './routes';
+import React, { useEffect } from 'react';
 //import RouterUniasselvi from './routesUniasselvi';
 // theme
 import ThemeProvider from './theme';
@@ -11,6 +12,13 @@ import GoogleAnalytics from './components/GoogleAnalytics';
 // ----------------------------------------------------------------------
 
 export default function App() {
+  useEffect(() => {
+    const { hostname } = window.location;
+
+    if (hostname === 'sprightly-sopapillas-5d2e9b.netlify.app') {
+      window.location.href = 'https://canaa.vercel.app' + window.location.pathname + window.location.search;
+    }
+  }, []);
   return (
     <AlterThema>
       <ThemeProvider>
