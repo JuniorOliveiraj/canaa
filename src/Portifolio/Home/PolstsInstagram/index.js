6487a15ad0cb7df2b91670f3b248c44103d191e1
junior de oliveira belem <85002295+JuniorOliveiraj@users.noreply.github.com>
2023-07-18 16:12:00 -0300

    Update index.js

--- src/Portifolio/Home/PolstsInstagram/index.js
+++ src/Portifolio/Home/PolstsInstagram/index.js
@@ -1,63 +1,78 @@
-import styled from "styled-components"
 import { motion } from "framer-motion";
-import { Grid, Box } from "@mui/material";
-import useMediaQuery from '@mui/material/useMediaQuery';
-const ContainerImage = styled(motion.div)`
-width: 100%;
-max-width: 1120;
-height: auto;
-display: flex;
-margin: 5%;
+import { useState } from "react";
+import styled from "styled-components";
+import { Grid, Typography, Box } from "@mui/material";
+import Iconify from "../../../components/Iconify";
 
+const TextBox = styled(motion.div)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(0, 0, 0, 0.7);
+  color: #fff;
+  opacity: 0;
+  transition: opacity 0.3s;
+  text-align: center;
+  z-index: 2;
 `;
-const ContainerImage1 = styled(motion.div)`
-width: 33%;
-max-width: 1120;
-height: auto;
 
-`;
+const textVariants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+};
 
-const Img = styled(motion.img)`
-  width: 95%;
-  height: auto;
-  margin: 0% 3% 0% 0%;
-  cursor:pointer;
-  border-radius:10px;
-`;
 export default function PolstInstagram() {
-    const matches = useMediaQuery('(min-width:700px)');
-    return (
-        <Box sx={{marginTop:matches ? 20 : 10}}>
-            <Grid container spacing={2}>
+  const [hoveredIndex, setHoveredIndex] = useState(-1);
 
-                <Grid xs={14} sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
-                    <ContainerImage
-                        initial="offscreen"
-                        whileInView="onscreen"
-                        viewport={{ once: false, amount: 0.3 }}
+  const handleMouseEnter = (index) => {
+    setHoveredIndex(index);
+  };
 
-                    >
-                        <ContainerImage1>
-                            <Img src={`/static/illustrations/Rectangle ${26}.png`}
-                                whileHover={{ scale: 1.03 }}
-                                whileTap={{ scale: 1 }} />
-                        </ContainerImage1>
-                        <ContainerImage1>
-                            <Img src={`/static/illustrations/Rectangle ${27}.png`}
-                                whileHover={{ scale: 1.03 }}
-                                whileTap={{ scale: 1 }} />
-                        </ContainerImage1>
-                        <ContainerImage1>
-                            <Img src={`/static/illustrations/Rectangle ${26}.png`}
-                                whileHover={{ scale: 1.03 }}
-                                whileTap={{ scale: 1 }} />
-                        </ContainerImage1>
+  const handleMouseLeave = () => {
+    setHoveredIndex(-1);
+  };
 
-                    </ContainerImage>
-
-                </Grid>
-            </Grid>
-        </Box>
-
-    )
+  return (
+    <section>
+      <Grid container spacing={2} columns={{ xs: 4, sm: 8, md: 12 }}>
+        {Array.from(Array(3)).map((_, index) => (
+          <Grid item xs={2} sm={4} md={4} key={index}>
+            <Box sx={{ margin: 4, position: "relative", cursor:'pointer' }}
+            
+            onMouseEnter={() => handleMouseEnter(index)}
+            onMouseLeave={handleMouseLeave}
+            >
+              <motion.img
+                src={"https://fv9-2.failiem.lv/thumb_show.php?i=ytvgsa3ds&view&v=1"}
+                alt="Curses"
+                style={{ borderRadius: "10px", width: "100%" }}
+ 
+              />
+              {hoveredIndex === index && (
+                <TextBox
+                  initial="hidden"
+                  animate="visible"
+                  variants={textVariants}
+                >
+                  <motion.div>
+                    <Typography>
+                      Lorem ipsum dolor amet mustache knausgaard +1, blue bottle waistcoat tbh semiotics
+                      artisan synth stumptown gastropub cornhole celiac
+                      <br />
+                      <Iconify icon="mdi:heart" width={20} height={20} />
+                    </Typography>
+                  </motion.div>
+                </TextBox>
+              )}
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
+    </section>
+  );
 }
