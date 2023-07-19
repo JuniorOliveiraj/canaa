42b35793162816d2014e3b04611e23e3a9835761
junior de oliveira belem <85002295+JuniorOliveiraj@users.noreply.github.com>
2023-07-19 14:43:39 -0300

    Update BlogCardPosts.js

--- src/components/_dashboard/blog/BlogCardPosts.js
+++ src/components/_dashboard/blog/BlogCardPosts.js
@@ -1,10 +1,12 @@
 
 
-import { BlogPostCard, BlogPostsSearch,BlogPostsSort } from '../../../sections/@dashboard/blog';
+import { BlogPostCard, BlogPostsSearch, BlogPostsSort } from '../../../sections/@dashboard/blog';
 // mock
-import { Grid, Box , Stack} from '@mui/material';
+import { Grid, Box, Stack } from '@mui/material';
 import POSTS from '../../../_mock/blog';
-
+import { useEffect, useState } from 'react';
+import urlApi from '../../../_mock/url';
+import axios from 'axios';
 // ----------------------------------------------------------------------
 
 const SORT_OPTIONS = [
@@ -14,18 +16,34 @@ const SORT_OPTIONS = [
 ];
 
 
-export default function BlogCardPosts({path}) {
+export default function BlogCardPosts({ path }) {
+    const [blogs, setBlogs] = useState([])
+    useEffect(() => {
+        const ListBlog = async () => {
+            axios.get(`${urlApi}/blog/list`)
+                .then((response) => {
+                    console.log(response.data.BLOG)
+                    setBlogs(response.data.BLOG)
+                    console.log(POSTS)
+
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        }
+        ListBlog()
+    }, []);
     return (
         <Box>
 
             <Stack mb={5} direction="row" alignItems="center" justifyContent="space-between">
-                <BlogPostsSearch posts={POSTS} />
+                <BlogPostsSearch posts={blogs} />
                 <BlogPostsSort options={SORT_OPTIONS} />
             </Stack>
 
             <Grid container spacing={3}>
-                {POSTS.map((post, index) => (
-                    <BlogPostCard key={post.id} post={post} index={index} path={path}/>
+                {blogs.map((post, index) => (
+                    <BlogPostCard key={post.id} post={post} index={index} path={path} />
                 ))}
             </Grid>
         </Box>
