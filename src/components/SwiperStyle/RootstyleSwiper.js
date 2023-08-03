c9e2169e4f431eda28b0a69e58f50927db0a58c8
junior de oliveira belem <junioroliveira.belem@gmail.com>
2023-08-03 19:26:29 -0300

    teste

--- src/components/SwiperStyle/RootstyleSwiper.js
+++ src/components/SwiperStyle/RootstyleSwiper.js
@@ -2,7 +2,6 @@ import { styled } from '@mui/material';
 import 'swiper/css/pagination';
 import 'swiper/css';
 import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 // const RootstyleSwiper = styled('div')(({ theme }) => ({
 //   '& .swiper-pagination-bullet-active': {
 //     color: '#fff',
@@ -17,27 +16,7 @@ import 'swiper/css/pagination';
   
 const RootstyleSwiper = styled('div')(({ theme }) => {
    
-  return {
-    '& .swiper-pagination-bullet': {
-      color: '#fff',
-      background: theme.palette.primary.main,
-     
-    } ,
-    '&  .swiper-pagination-bullet-active': {
-      color: '#fff',
-      background: theme.palette.primary.main,
-      width: '10px',
-      height: '10px',
-    } ,
-    '& .swiper-button-prev': {
-      color:  theme.palette.primary.main,
-    } ,
-    '& .swiper-button-next': {
-      color:  theme.palette.primary.main,
-    } ,
-    
-    
-  };
+ 
 });
   
   export default RootstyleSwiper;
