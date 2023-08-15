b2ccd81316cd0307205fee1fa1f2742a680f1be5
junior de oliveira belem <85002295+JuniorOliveiraj@users.noreply.github.com>
2023-08-15 15:11:51 -0300

    Update index.js

--- src/components/_external-pages/payment/index.js
+++ src/components/_external-pages/payment/index.js
@@ -2,3 +2,5 @@ export { default as PaymentBillingAddress } from './PaymentBillingAddress';
 export { default as PaymentMethods } from './PaymentMethods';
 export { default as PaymentNewCardForm } from './PaymentNewCardForm';
 export { default as PaymentSummary } from './PaymentSummary';
+
+
