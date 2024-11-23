4b9b406b9ff4019c2df98330aacd14809cf829f9
Junior oliveira <junioroliveira.belem@gmail.com>
2024-11-23 12:11:17 -0300

    Tela de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,7 @@
 import { Link as RouterLink } from 'react-router-dom';
 // @mui
 import { styled } from '@mui/material/styles';
-import { Card, Link, Container, Typography } from '@mui/material';
+import { Card, Link, Container, Typography, Box } from '@mui/material';
 // hooks
 import useResponsive from '../hooks/useResponsive';
 // components
@@ -10,15 +10,41 @@ import Logo from '../components/Logo';
 // sections
 import { LoginForm } from '../sections/auth/login';
 import AuthSocial from '../sections/auth/AuthSocial';
-
+import { motion } from 'framer-motion';
 // ----------------------------------------------------------------------
 
 const RootStyle = styled('div')(({ theme }) => ({
+  position: 'relative',
+  overflow: 'hidden', // Evita o conteúdo exceder os limites
+  '&::before': {
+    content: '""',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    backgroundImage: 'url(https://free.minimals.cc/assets/background/overlay.jpg)',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    opacity: 0.2, // Controla a opacidade da imagem
+    zIndex: -1, // Garante que a imagem fique atrás do conteúdo
+  },
   [theme.breakpoints.up('md')]: {
     display: 'flex',
   },
 }));
 
+ 
+
+const ContentStyle = styled('div')(({ theme }) => ({
+  maxWidth: 480,
+  margin: 'auto',
+  minHeight: '100vh',
+  display: 'flex',
+  justifyContent: 'center',
+  flexDirection: 'column',
+  padding: theme.spacing(12, 0),
+}));
 const HeaderStyle = styled('header')(({ theme }) => ({
   top: 0,
   zIndex: 9,
@@ -31,27 +57,8 @@ const HeaderStyle = styled('header')(({ theme }) => ({
   justifyContent: 'space-between',
   [theme.breakpoints.up('md')]: {
     alignItems: 'flex-start',
-    padding: theme.spacing(7, 5, 0, 7),
+    padding: theme.spacing(1, 5, 0, 7),
   },
-}));
-
-const SectionStyle = styled(Card)(({ theme }) => ({
-  width: '100%',
-  maxWidth: 464,
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  margin: theme.spacing(2, 0, 2, 2),
-}));
-
-const ContentStyle = styled('div')(({ theme }) => ({
-  maxWidth: 480,
-  margin: 'auto',
-  minHeight: '100vh',
-  display: 'flex',
-  justifyContent: 'center',
-  flexDirection: 'column',
-  padding: theme.spacing(12, 0),
 }));
 
 // ----------------------------------------------------------------------
@@ -64,41 +71,48 @@ export default function Login() {
   return (
     <Page title="Login">
       <RootStyle>
-        <HeaderStyle>
+      <HeaderStyle>
           <Logo />
-          {smUp && (
-            <Typography variant="body2" sx={{ mt: { md: -2 } }}>
-              Don’t have an account? {''}
-              <Link variant="subtitle2" component={RouterLink} to="/register">
-                Get started
-              </Link>
-            </Typography>
-          )}
+
         </HeaderStyle>
-        {mdUp && (
-          <SectionStyle>
-            <Typography variant="h3" sx={{ px: 5, mt: 10, mb: 5 }}>
-              Hi, Welcome Back
-            </Typography>
-            <img src="/static/illustrations/illustration_login.png" alt="login" />
-          </SectionStyle>
-        )}
         <Container maxWidth="sm">
           <ContentStyle>
-            <Typography variant="h4" gutterBottom>
-              Sign in to Minimal
-            </Typography>
-            <Typography sx={{ color: 'text.secondary', mb: 5 }}>Enter your details below.</Typography>
-            <AuthSocial />
-            <LoginForm />
-            {!smUp && (
-              <Typography variant="body2" align="center" sx={{ mt: 3 }}>
-                Don’t have an account?{' '}
-                <Link variant="subtitle2" component={RouterLink} to="/register">
-                  Get started
-                </Link>
+          <motion.div
+              initial={{ rotateY: 50, opacity: 0 }}
+              animate={{ rotateY: 0, opacity: 1 }}
+              transition={{ duration: 0.5, ease: 'easeInOut' }}
+            >
+            <Card>
+              <Box sx={{margin:5, }}>
+              <Typography variant="h4" gutterBottom>
+                Sign now
               </Typography>
-            )}
+
+              <Typography sx={{ color: 'text.secondary', mb: 5 }}>Enter your details below.</Typography>
+              {smUp && (
+                <Typography variant="body2" sx={{ mt: { md: -2 } }}>
+                  Don’t have an account? {''}
+                  <Link variant="subtitle2" component={RouterLink} to="/register">
+                    Get started
+                  </Link>
+                </Typography>
+              )}
+              <br/>
+              <AuthSocial />
+              <LoginForm />
+              {!smUp && (
+                <Typography variant="body2" align="center" sx={{ mt: 3 }}>
+                  Don’t have an account?{' '}
+                  <Link variant="subtitle2" component={RouterLink} to="/register">
+                    Get started
+                  </Link>
+                </Typography>
+              )}
+
+
+              </Box>
+            </Card>
+            </motion.div>
           </ContentStyle>
         </Container>
       </RootStyle>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,7 @@
 import { Link as RouterLink } from 'react-router-dom';
 // @mui
 import { styled } from '@mui/material/styles';
-import { Card, Link, Container, Typography } from '@mui/material';
+import { Card, Link, Container, Typography, Box } from '@mui/material';
 // hooks
 import useResponsive from '../hooks/useResponsive';
 // components
@@ -10,10 +10,27 @@ import Logo from '../components/Logo';
 // sections
 import { RegisterForm } from '../sections/auth/register';
 import AuthSocial from '../sections/auth/AuthSocial';
+// Framer Motion
+import { motion } from 'framer-motion';
 
 // ----------------------------------------------------------------------
 
 const RootStyle = styled('div')(({ theme }) => ({
+  position: 'relative',
+  overflow: 'hidden', // Evita o conteúdo exceder os limites
+  '&::before': {
+    content: '""',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    backgroundImage: 'url(https://free.minimals.cc/assets/background/overlay.jpg)',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    opacity: 0.2, // Controla a opacidade da imagem
+    zIndex: -1, // Garante que a imagem fique atrás do conteúdo
+  },
   [theme.breakpoints.up('md')]: {
     display: 'flex',
   },
@@ -31,17 +48,8 @@ const HeaderStyle = styled('header')(({ theme }) => ({
   justifyContent: 'space-between',
   [theme.breakpoints.up('md')]: {
     alignItems: 'flex-start',
-    padding: theme.spacing(7, 5, 0, 7),
+    padding: theme.spacing(1, 5, 0, 7),
   },
-}));
-
-const SectionStyle = styled(Card)(({ theme }) => ({
-  width: '100%',
-  maxWidth: 464,
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  margin: theme.spacing(2, 0, 2, 2),
 }));
 
 const ContentStyle = styled('div')(({ theme }) => ({
@@ -66,57 +74,61 @@ export default function Register() {
       <RootStyle>
         <HeaderStyle>
           <Logo />
-          {smUp && (
-            <Typography variant="body2" sx={{ mt: { md: -2 } }}>
-              Already have an account? {''}
-              <Link variant="subtitle2" component={RouterLink} to="/login">
-                Login
-              </Link>
-            </Typography>
-          )}
         </HeaderStyle>
-
-        {mdUp && (
-          <SectionStyle>
-            <Typography variant="h3" sx={{ px: 5, mt: 10, mb: 5 }}>
-              Manage the job more effectively with Portifolio
-            </Typography>
-            <img alt="register" src="/static/illustrations/illustration_register.png" />
-          </SectionStyle>
-        )}
 
         <Container>
           <ContentStyle>
-            <Typography variant="h4" gutterBottom>
-              Get started absolutely free.
-            </Typography>
+            {/* Adicionando o efeito de flip no Card */}
+            <motion.div
+              initial={{ rotateY: 50, opacity: 0 }}
+              animate={{ rotateY: 0, opacity: 1 }}
+              transition={{ duration: 0.5, ease: 'easeInOut' }}
+            >
+              <Card>
+                <Box sx={{ padding: 5 }}>
+                  <Typography variant="h4" gutterBottom>
+                    Get started absolutely free.
+                  </Typography>
 
-            <Typography sx={{ color: 'text.secondary', mb: 5 }}>Free forever. No credit card needed.</Typography>
+                  <Typography sx={{ color: 'text.secondary', mb: 5 }}>
+                    Free forever. No credit card needed.
+                  </Typography>
+                  {smUp && (
+                    <Typography variant="body2" sx={{ mt: { md: -2 } }}>
+                      Already have an account? {''}
+                      <Link variant="subtitle2" component={RouterLink} to="/login">
+                        Login
+                      </Link>
+                    </Typography>
+                  )}
+                  <br />
+                  <AuthSocial />
 
-            <AuthSocial />
+                  <RegisterForm />
 
-            <RegisterForm />
+                  <Typography variant="body2" align="center" sx={{ color: 'text.secondary', mt: 3 }}>
+                    By registering, I agree to junior&nbsp;
+                    <Link underline="always" color="text.primary" href="#">
+                      Terms of Service
+                    </Link>
+                    {''}and{''}
+                    <Link underline="always" color="text.primary" href="#">
+                      Privacy Policy
+                    </Link>
+                    .
+                  </Typography>
 
-            <Typography variant="body2" align="center" sx={{ color: 'text.secondary', mt: 3 }}>
-              By registering, I agree to junior&nbsp;
-              <Link underline="always" color="text.primary" href="#">
-                Terms of Service
-              </Link>
-              {''}and{''}
-              <Link underline="always" color="text.primary" href="#">
-                Privacy Policy
-              </Link>
-              .
-            </Typography>
-
-            {!smUp && (
-              <Typography variant="body2" sx={{ mt: 3, textAlign: 'center' }}>
-                Already have an account?{' '}
-                <Link variant="subtitle2" to="/login" component={RouterLink}>
-                  Login
-                </Link>
-              </Typography>
-            )}
+                  {!smUp && (
+                    <Typography variant="body2" sx={{ mt: 3, textAlign: 'center' }}>
+                      Already have an account?{' '}
+                      <Link variant="subtitle2" to="/login" component={RouterLink}>
+                        Login
+                      </Link>
+                    </Typography>
+                  )}
+                </Box>
+              </Card>
+            </motion.div>
           </ContentStyle>
         </Container>
       </RootStyle>
